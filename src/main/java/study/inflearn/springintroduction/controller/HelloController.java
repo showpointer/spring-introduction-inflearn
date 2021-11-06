89b6ebcfae25b3d2fbdf1e9d0ae7203e85b7e7bb
showpointer <wanho92@gmail.com>
2021-11-06 18:43:04 +0900

    feat: thymeleaf template 템플릿엔진 동작확인

---
+++ src/main/java/study/inflearn/springintroduction/controller/HelloController.java
@@ -0,0 +1,16 @@
+package study.inflearn.springintroduction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring hello!");
+
+        return "hello";
+    }
+}
