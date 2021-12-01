1a91993e1866985331df2b7ab5fe4eb3b1f799f0
showpointer <wanho92@gmail.com>
2021-12-01 22:37:39 +0900

    feat: 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/study/inflearn/springintroduction/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package study.inflearn.springintroduction.repository;
+
+import study.inflearn.springintroduction.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
