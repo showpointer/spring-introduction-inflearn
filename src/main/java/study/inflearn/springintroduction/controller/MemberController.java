f6135231188624ed355c9832e16ff4349347d326
showpointer <wanho92@gmail.com>
2021-12-06 23:33:32 +0900

    feat: 회원 웹 기능 - 조회

--- src/main/java/study/inflearn/springintroduction/controller/MemberController.java
+++ src/main/java/study/inflearn/springintroduction/controller/MemberController.java
@@ -2,10 +2,13 @@ package study.inflearn.springintroduction.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.inflearn.springintroduction.domain.Member;
 import study.inflearn.springintroduction.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
