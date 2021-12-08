1d756a525925533b136eaa699534533169a02e3b
showpointer <wanho92@gmail.com>
2021-12-08 22:28:02 +0900

    feat: 순수 JDBC

--- src/main/java/study/inflearn/springintroduction/SpringConfig.java
+++ src/main/java/study/inflearn/springintroduction/SpringConfig.java
@@ -2,12 +2,20 @@ package study.inflearn.springintroduction;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.inflearn.springintroduction.repository.JdbcMemberRepository;
 import study.inflearn.springintroduction.repository.MemberRepository;
-import study.inflearn.springintroduction.repository.MemoryMemberRepository;
 import study.inflearn.springintroduction.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
